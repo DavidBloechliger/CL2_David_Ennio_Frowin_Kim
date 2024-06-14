9407a9a5780d848d3cfae14bada779bf0bbe3409
KimFelber <167539439+KimFelber@users.noreply.github.com>
2024-06-14 09:51:46 +0200

    Update forms.js

--- js/forms.js
+++ js/forms.js
@@ -1,20 +1,37 @@
-
-
 // (1) Variablen initialisieren
 const form_group = document.getElementById("form-group");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
-const message = document.getElementById("message");
 const emailField = document.getElementById("e-mail");
+const message = document.getElementById("message");
 const submit = document.getElementById("submit");
+const errorMessage = document.getElementById("error-message");
+
+function isValidEmail(email) {
+    return email.includes('@');
+}
 
 function emailforms() {
+    let isValid = true;
+    errorMessage.innerHTML = ""; // Leert die Fehlermeldungen
 
-    if (emailField.includes('@')) {
-        alert('Gib eine gültige E-Mail-Adresse ein.')
-        return false
+    // Überprüfung der E-Mail-Adresse
+    if (!isValidEmail(emailField.value)) {
+        errorMessage.innerHTML += 'Gib eine gültige E-Mail-Adresse ein.<br>';
+        isValid = false;
     }
-    return true
+
+    if (fname.value.length < 2) {
+        errorMessage.innerHTML += 'Der Vorname muss mindestens 3 Zeichen lang sein.<br>';
+        isValid = false;
+    }
+
+    if (lname.value.length < 2) {
+        errorMessage.innerHTML += 'Der Nachname muss mindestens 3 Zeichen lang sein.<br>';
+        isValid = false;
+    }
+
+    return isValid;
 }
 
 submit.addEventListener("click", async (event) => {
@@ -24,8 +41,11 @@ submit.addEventListener("click", async (event) => {
 
 
 const onClickSubmit = async () => {
-    // Speichert die Daten in der Datenbank
-    await databaseClient.insertInto("form_data", {
-        mail: emailField.value,
-    });
+    const isValid = emailforms();
+    if (isValid) {
+        // Speichert die Daten in der Datenbank
+        await databaseClient.insertInto("form_data", {
+            mail: emailforms.value,
+        });
+    }
 }
