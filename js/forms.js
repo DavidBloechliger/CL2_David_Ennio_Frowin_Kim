07870e77da8aaaab15f847d6e0e024ad99f5022f
DavidBloechliger <88771808+DavidBloechliger@users.noreply.github.com>
2024-06-14 09:20:42 +0200

    Add files via upload

--- js/forms.js
+++ js/forms.js
@@ -1,37 +1,20 @@
+
+
 // (1) Variablen initialisieren
 const form_group = document.getElementById("form-group");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
-const emailField = document.getElementById("e-mail");
 const message = document.getElementById("message");
+const emailField = document.getElementById("e-mail");
 const submit = document.getElementById("submit");
-const errorMessage = document.getElementById("error-message");
-
-function isValidEmail(email) {
-    return email.includes('@');
-}
 
 function emailforms() {
-    let isValid = true;
-    errorMessage.innerHTML = ""; // Leert die Fehlermeldungen
 
-    // Überprüfung der E-Mail-Adresse
-    if (!isValidEmail(emailField.value)) {
-        errorMessage.innerHTML += 'Gib eine gültige E-Mail-Adresse ein.<br>';
-        isValid = false;
+    if (emailField.includes('@')) {
+        alert('Gib eine gültige E-Mail-Adresse ein.')
+        return false
     }
-
-    if (fname.value.length < 2) {
-        errorMessage.innerHTML += 'Der Vorname muss mindestens 3 Zeichen lang sein.<br>';
-        isValid = false;
-    }
-
-    if (lname.value.length < 2) {
-        errorMessage.innerHTML += 'Der Nachname muss mindestens 3 Zeichen lang sein.<br>';
-        isValid = false;
-    }
-
-    return isValid;
+    return true
 }
 
 submit.addEventListener("click", async (event) => {
@@ -41,11 +24,8 @@ submit.addEventListener("click", async (event) => {
 
 
 const onClickSubmit = async () => {
-    const isValid = emailforms();
-    if (isValid) {
-        // Speichert die Daten in der Datenbank
-        await databaseClient.insertInto("form_data", {
-            mail: emailforms.value,
-        });
-    }
+    // Speichert die Daten in der Datenbank
+    await databaseClient.insertInto("form_data", {
+        mail: emailField.value,
+    });
 }
